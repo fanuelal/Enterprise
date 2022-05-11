3d68047e28aba6d038e527986c6c5a4bb4f1e638
fanuelal <fanuelalmawstop@gmail.com>
2022-05-12 00:29:49 +0300

    buyer update

---
+++ Buyer.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * @author Hailat66
+ * @version 1.0
+ * @created 11-May-2022 9:29:45 PM
+ */
+public class Buyer extends Customer {
+
+	private int PostedHouse;
+	public Seller m_Seller;
+
+	public Buyer(){
+
+	}
+
+	public void finalize() throws Throwable {
+		super.finalize();
+	}
+	public void view_detailHouse(){
+
+	}
+
+	public void view_Detailseller(){
+
+	}
+
+	public void Visit_tosoldHouse(){
+
+	}
+}//end Buyer
